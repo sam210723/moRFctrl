1cbe1977f56895a09e202f4f1a1ddfaedf656c90
Sam <sam210723@hotmail.com>
2019-01-27 18:46:28 +1000

    moRFeus class rewrite

--- moRFctrl/moRFeus.cs
+++ moRFctrl/moRFeus.cs
@@ -8,40 +8,28 @@ namespace moRFctrl
     /// </summary>
     static class moRFeus
     {
-        static byte[] typeGet = { 0x00, 0x72 };
-        static byte[] typeSet = { 0x00, 0x77 };
-        static byte[] paramFreq = { 0x81 };
-        static byte[] paramFunc = { 0x82 };
-        static byte[] paramMixI = { 0x83 };
-        static byte[] paramBias = { 0x84 };
+        static readonly byte[] typeGet = { 0x00, 0x72 };
+        static readonly byte[] typeSet = { 0x00, 0x77 };
+        static readonly byte[] paramFreq = { 0x81 };
+        static readonly byte[] paramFunc = { 0x82 };
+        static readonly byte[] paramMixI = { 0x83 };
+        static readonly byte[] paramBias = { 0x84 };
+        static readonly byte[] nullBytes = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
 
-        public static int FUNC_MIXER = 0;
-        public static int FUNC_GENERATOR = 1;
-        public static int BIAS_OFF = 0;
-        public static int BIAS_ON = 1;
+        public static readonly int FUNC_MIXER = 0;
+        public static readonly int FUNC_GENERATOR = 1;
+        public static readonly int BIAS_OFF = 0;
+        public static readonly int BIAS_ON = 1;
 
         #region Set
         /// <summary>
         /// Set generator frequency
         /// </summary>
         /// <param name="freq">Frequency in Hz</param>
-        public static void SetFrequency(UInt64 freq)
+        public static void SetFrequency(ulong freq)
         {
-            byte[] type = typeSet;
-            byte[] param = paramFreq;
-            byte[] val = BitConverter.GetBytes(freq);
-            byte[] valPadded = new byte[8];
-
-            // Create padded value array
-            byte[] nullByte = { 0x00 };
-            for (int i = val.Length; i < 8; i++)
-            {
-                Buffer.BlockCopy(nullByte, 0, valPadded, 0, nullByte.Length);
-            }
-            Buffer.BlockCopy(val, 0, valPadded, 0, val.Length);
-            valPadded = valPadded.Reverse().ToArray();
-
-            SendCommand(type, param, valPadded);
+            byte[] padded = nullPad(BitConverter.GetBytes(freq), 8);
+            SendCommand(typeSet, paramFreq, padded);
         }
 
         /// <summary>
@@ -49,23 +37,8 @@ namespace moRFctrl
         /// </summary>
         public static void SetFunction(int func)
         {
-            byte[] type = typeSet;
-            byte[] param = paramFunc;
-
-            if (func == FUNC_MIXER)
-            {
-                byte[] val = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-                SendCommand(type, param, val);
-            }
-            else if (func == FUNC_GENERATOR)
-            {
-                byte[] val = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 };
-                SendCommand(type, param, val);
-            }
-            else
-            {
-                return;
-            }
+            byte[] padded = nullPad(BitConverter.GetBytes(func), 8);
+            SendCommand(typeSet, paramFunc, padded);
         }
 
         /// <summary>
@@ -74,21 +47,8 @@ namespace moRFctrl
         /// <param name="i">0 to 7</param>
         public static void SetMixerCurrent(int mixI)
         {
-            byte[] type = typeSet;
-            byte[] param = paramMixI;
-            byte[] val = BitConverter.GetBytes(mixI);
-            byte[] valPadded = new byte[8];
-
-            // Create padded value array
-            byte[] nullByte = { 0x00 };
-            for (int i = val.Length; i < 8; i++)
-            {
-                Buffer.BlockCopy(nullByte, 0, valPadded, 0, nullByte.Length);
-            }
-            Buffer.BlockCopy(val, 0, valPadded, 0, val.Length);
-            valPadded = valPadded.Reverse().ToArray();
-
-            SendCommand(type, param, valPadded);
+            byte[] padded = nullPad(BitConverter.GetBytes(mixI), 8);
+            SendCommand(typeSet, paramMixI, padded);
         }
 
         /// <summary>
@@ -96,23 +56,8 @@ namespace moRFctrl
         /// </summary>
         public static void SetBiasTee(int bias)
         {
-            byte[] type = typeSet;
-            byte[] param = paramBias;
-
-            if (bias == BIAS_OFF)
-            {
-                byte[] val = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-                SendCommand(type, param, val);
-            }
-            else if (bias == BIAS_ON)
-            {
-                byte[] val = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01 };
-                SendCommand(type, param, val);
-            }
-            else
-            {
-                return;
-            }
+            byte[] padded = nullPad(BitConverter.GetBytes(bias), 8);
+            SendCommand(typeSet, paramBias, padded);
         }
         #endregion
 
@@ -120,41 +65,72 @@ namespace moRFctrl
         /// <summary>
         /// Get generator frequency
         /// </summary>
-        public static void GetFrequency()
+        public static ulong GetFrequency()
         {
-            byte[] val = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            SendCommand(typeGet, paramFreq, nullBytes);
 
-            SendCommand(typeGet, paramFreq, val);
+            ulong freq = ParseReport(Program.HIDClass.ReceiveHIDReport());
+            return freq;
         }
 
         /// <summary>
         /// Get mode (mixer/generator)
         /// </summary>
-        public static void GetFunction()
+        public static int GetFunction()
         {
-            byte[] val = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            SendCommand(typeGet, paramFunc, nullBytes);
 
-            SendCommand(typeGet, paramFunc, val);
+            int func = (int) ParseReport(Program.HIDClass.ReceiveHIDReport());
+            return func;
         }
 
         /// <summary>
         /// Get mixer current
         /// </summary>
-        public static void GetMixerCurrent()
+        public static int GetMixerCurrent()
         {
-            byte[] val = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            SendCommand(typeGet, paramMixI, nullBytes);
 
-            SendCommand(typeGet, paramMixI, val);
+            int mixI = (int) ParseReport(Program.HIDClass.ReceiveHIDReport());
+            return mixI;
         }
 
         /// <summary>
         /// Get bias tee (on/off)
         /// </summary>
-        public static void GetBiasTee()
+        public static bool GetBiasTee()
         {
-            byte[] val = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+            SendCommand(typeGet, paramBias, nullBytes);
 
-            SendCommand(typeGet, paramBias, val);
+            int bias = (int) ParseReport(Program.HIDClass.ReceiveHIDReport());
+
+            if (bias == BIAS_OFF)
+            {
+                return false;
+            }
+            else if (bias == BIAS_ON)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Get all parameters
+        /// </summary>
+        public static moRFeusState GetAll()
+        {
+            moRFeusState state = new moRFeusState();
+
+            state.Frequency = GetFrequency();
+            state.Function = GetFunction();
+            state.MixerCurrent = GetMixerCurrent();
+            state.BiasTee = GetBiasTee();
+
+            return state;
         }
         #endregion
 
@@ -162,72 +138,89 @@ namespace moRFctrl
         /// Pasrse HID input report
         /// </summary>
         /// <param name="r">Input report</param>
-        public static void ParseReport(byte[] r)
+        public static ulong ParseReport(byte[] report)
         {
-            //Console.WriteLine("\nFrom HID:");
-            //Console.WriteLine("Raw: " + BitConverter.ToString(r).Replace("-", string.Empty));
-            //Console.WriteLine("Length: " + r.Length);
+            Tools.Debug("\nFrom HID:");
+            Tools.Debug("Raw: " + BitConverter.ToString(report).Replace("-", string.Empty));
+            Tools.Debug("Length: " + report.Length);
 
-            byte type = r[1];
-            byte param = r[2];
-            byte[] val = new byte[8];
-            Array.Copy(r, 3, val, 0, 8);
+            // Report fields
+            byte type = report[1];
+            byte param = report[2];
+            byte[] value = new byte[8];
+            Array.Copy(report, 3, value, 0, 8);
 
+            // Detect report type
             if (type == typeGet[1])
             {
-                //Console.WriteLine("Report Type: GET");
+                Tools.Debug("Type: GET");
             }
             else if (type == typeSet[1])
             {
-                //Console.WriteLine("Report Type: SET");
-
-                // Only parse set report for frequency
-                if (param != paramFreq[0])
-                {
-                    return;
-                }
+                Tools.Debug("Type: SET");
             }
 
+            // Detect parameter
             if (param == paramFreq[0])
             {
-                //Console.WriteLine("Parameter: FREQUENCY");
+                Tools.Debug("Parameter: FREQUENCY");
 
-                Program.MainClass.Frequency = BitConverter.ToUInt64(val.Reverse().ToArray(), 0).ToString();
-                // Set class frequency global
+                ulong freq = BitConverter.ToUInt64(value.Reverse().ToArray(), 0);
+                Tools.Debug("Value: " + freq + " Hz");
+                
+                return freq;
             }
             else if (param == paramFunc[0])
             {
-                //Console.WriteLine("Parameter: FUNCTION");
+                Tools.Debug("Parameter: FUNCTION");
 
-                Program.MainClass.Function = BitConverter.ToInt16(val.Reverse().ToArray(), 0);
+                ulong func = BitConverter.ToUInt64(value.Reverse().ToArray(), 0);
+
+                if (func == (ulong) FUNC_GENERATOR)
+                {
+                    Tools.Debug("Value: GENERATOR");
+                }
+                else if (func == (ulong) FUNC_MIXER)
+                {
+                    Tools.Debug("Value: MIXER");
+                }
+
+                return func;
             }
             else if (param == paramMixI[0])
             {
-                //Console.WriteLine("Parameter: MIXER CURRENT");
+                Tools.Debug("Parameter: MIXER CURRENT");
 
-                Program.MainClass.MixerCurrent = BitConverter.ToInt16(val.Reverse().ToArray(), 0);
+                ulong mixI = BitConverter.ToUInt64(value.Reverse().ToArray(), 0);
+                Tools.Debug("Value: " + mixI + "/7");
+
+                return mixI;
             }
             else if (param == paramBias[0])
             {
-                //Console.WriteLine("Parameter: BIAS TEE");
+                Tools.Debug("Parameter: BIAS TEE");
 
-                if (BitConverter.ToInt16(val.Reverse().ToArray(), 0) == BIAS_OFF)
+                ulong bias = BitConverter.ToUInt64(value.Reverse().ToArray(), 0);
+
+                if (bias == (ulong) BIAS_OFF)
                 {
-                    Program.MainClass.BiasTee = false;
+                    Tools.Debug("Value: OFF");
                 }
-                else
+                else if (bias == (ulong) BIAS_ON)
                 {
-                    Program.MainClass.BiasTee = true;
+                    Tools.Debug("Value: ON");
                 }
+
+                return bias;
             }
 
-            //Console.WriteLine("Value: " + BitConverter.ToString(val).Replace("-", string.Empty));
+            return 65535;
         }
 
         /// <summary>
         /// Build moRFeus protocol command and send as HID report
         /// </summary>
-        static void SendCommand(byte[] type, byte[] param, byte[] val)
+        private static void SendCommand(byte[] type, byte[] param, byte[] val)
         {
             byte[] pad = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
             byte[] cmd = new byte[type.Length + param.Length + val.Length + pad.Length];
@@ -237,10 +230,85 @@ namespace moRFctrl
             Buffer.BlockCopy(val, 0, cmd, type.Length + param.Length, val.Length);
             Buffer.BlockCopy(pad, 0, cmd, type.Length + param.Length + val.Length, pad.Length);
 
-            //Console.WriteLine("\nTo HID:");
-            //Console.WriteLine("Raw: " + BitConverter.ToString(cmd).Replace("-", string.Empty));
-            //Console.WriteLine("Length: " + cmd.Length);
+            Tools.Debug("\nTo HID:");
+            Tools.Debug("Raw: " + BitConverter.ToString(cmd).Replace("-", string.Empty));
+            Tools.Debug("Length: " + cmd.Length);
+
+            // Detect report type
+            if (type == typeGet)
+            {
+                Tools.Debug("Type: GET");
+            }
+            else if (type == typeSet)
+            {
+                Tools.Debug("Type: SET");
+            }
+
+            // Detect parameter
+            if (param == paramFreq)
+            {
+                Tools.Debug("Parameter: FREQUENCY");
+
+                ulong freq = BitConverter.ToUInt64(val.Reverse().ToArray(), 0);
+                Tools.Debug("Value: " + freq + " Hz");
+            }
+            else if (param == paramFunc)
+            {
+                Tools.Debug("Parameter: FUNCTION");
+            }
+            else if (param == paramMixI)
+            {
+                Tools.Debug("Parameter: MIXER CURRENT");
+            }
+            else if (param == paramBias)
+            {
+                Tools.Debug("Parameter: BIAS TEE");
+            }
+
             Program.HIDClass.WriteHIDReport(cmd);
+        }
+
+        #region Tools
+        /// <summary>
+        /// Create padded byte array
+        /// </summary>
+        /// <returns>Byte array</returns>
+        static byte[] nullPad(byte[] value, int len)
+        {
+            byte[] nullByte = { 0x00 };
+            byte[] padded = new byte[len];
+
+            // Create padding
+            for (int i = value.Length; i < len; i++)
+            {
+                Buffer.BlockCopy(nullByte, 0, padded, 0, nullByte.Length);
+            }
+
+            // Add value to padding
+            Buffer.BlockCopy(value, 0, padded, 0, value.Length);
+
+            return padded.Reverse().ToArray();
+        }
+        #endregion
+    }
+
+
+    /// <summary>
+    /// Status of all parameters in one struct
+    /// </summary>
+    public struct moRFeusState
+    {
+        public ulong Frequency;
+        public int Function;
+        public int MixerCurrent;
+        public bool BiasTee;
+
+        public moRFeusState(ulong freq, int func, int mixi, bool bias)
+        {
+            Frequency = freq;
+            Function = func;
+            MixerCurrent = mixi;
+            BiasTee = bias;
         }
     }
 }
